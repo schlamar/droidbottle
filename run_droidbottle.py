459f7c8cfc401fbda9fa2ae22f5db0e8047bd4de
ms4py <marc.schlaich@googlemail.com>
2011-06-02 11:53:26 +0200

    findout wlan ip

--- run_droidbottle.py
+++ run_droidbottle.py
@@ -1,7 +1,23 @@
+import re
+from subprocess import Popen, PIPE
+
+import bottle
 from bottle import route, run
 
+bottle.debug(True)
+
+def get_ip():
+    out,_ = Popen('netcfg', stdout=PIPE).communicate()
+    pattern = re.compile('tiwlan0\s+UP\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+    match = pattern.search(out)
+    if match is not None:
+        return match.group(1)
+
+@route('/')
 @route('/:name')
 def index(name='World'):
     return '<b>Hello %s!</b>' % name
 
-run(host='localhost', port=8080)
+if __name__ == '__main__':
+    print get_ip()
+    run(host='', port=8080)
