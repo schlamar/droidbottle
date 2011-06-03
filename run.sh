1773a681e30617b0871de45ac12adaae6aa32bf1
ms4py <marc.schlaich@googlemail.com>
2011-06-03 18:12:06 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-bash install.sh
 adb shell am start -a com.googlecode.android_scripting.action.LAUNCH_FOREGROUND_SCRIPT -n com.googlecode.android_scripting/.activity.ScriptingLayerServiceLauncher -e com.googlecode.android_scripting.extra.SCRIPT_PATH /sdcard/sl4a/scripts/run_droidbottle.py
 adb forward tcp:8080 tcp:8080
 sleep 13
